b831b8af993e5ac1c1fada46ba450fda1de576a2
fpsacha <sachacnt@gmail.com>
2026-09-08 10:22:17 -0400

    Release v1.2.19

--- pz-mod/PanelBridge/media/lua/server/PanelBridge.lua
+++ pz-mod/PanelBridge/media/lua/server/PanelBridge.lua
@@ -1,7 +1,7 @@
 ---@diagnostic disable: undefined-global, deprecated
 --[[
     PanelBridge - Server-side mod for Zomboid Control Panel
-    Version: 1.7.56
+    Version: 1.7.57
 
     This mod enables external control panel communication with the PZ server.
     Communication happens via JSON files in the server save folder.
@@ -505,7 +505,7 @@
 local json
 
 local PanelBridge = {
-    VERSION = "1.7.56",
+    VERSION = "1.7.57",
     PROTOCOL_VERSION = "queue-v1",
     CHECK_INTERVAL = 250, -- milliseconds (fast command polling)
     lastCheck = 0,
